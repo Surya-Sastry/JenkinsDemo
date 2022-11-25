afe728cbf844063d92b2394c8b904eccf478842f
Surya Sastry <78991131+Surya-Sastry@users.noreply.github.com>
2022-11-25 11:52:16 +0530

    Added Fail test cases

--- FailTest.py
+++ FailTest.py
@@ -7,13 +7,13 @@ class TestAdd(unittest.TestCase):
     def test_fail_simple(self):
         a = 2
         b = 3
-        c = add(a, b)
-        self.assertEqual(c, -5)
+        c = mul(a, b)
+        self.assertEqual(c, 5)
 
     def test_fail_medium(self):
-        a = 12345678987654321
-        b = 98765432123456789
-        c = add(a, b)
+        a = 12
+        b = 9
+        c = mul(a, b)
         self.assertEqual(c, 0)
 
 
